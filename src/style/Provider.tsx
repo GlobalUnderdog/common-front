75e4917edc5c52776eab74236c95cf9d0288211d
Lukas Danin <lukas.danin@gmail.com>
2020-11-09 09:14:36 -0300

    Fixes headers global theme

--- src/style/Provider.tsx
+++ src/style/Provider.tsx
@@ -7,6 +7,7 @@ import { ScreenClassProvider, setConfiguration } from 'react-grid-system'
 import { ThemeProps } from './theme'
 import { NeedsChildrenProps } from './util'
 import { useTheme } from './emotion'
+import { css } from './emotion'
 
 const Children: React.FC = ({ children }) => {
   const theme = useTheme()
@@ -14,17 +15,65 @@ const Children: React.FC = ({ children }) => {
   return (
     <Fragment>
       <Global styles={theme.global.css(theme)} />
-      <Global styles={theme.font.h1h6.css(theme)} />
-      <Global styles={theme.font.h1.css(theme)} />
-      <Global styles={theme.font.h2.css(theme)} />
-      <Global styles={theme.font.h3.css(theme)} />
-      <Global styles={theme.font.h4.css(theme)} />
-      <Global styles={theme.font.h5.css(theme)} />
-      <Global styles={theme.font.h6.css(theme)} />
+      <Global
+        styles={css`
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            ${theme.font.h1h6.css(theme)}
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          h1 {
+            ${theme.font.h1.css(theme)}
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          h2 {
+            ${theme.font.h2.css(theme)}
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          h3 {
+            ${theme.font.h3.css(theme)}
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          h4 {
+            ${theme.font.h4.css(theme)}
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          h5 {
+            ${theme.font.h5.css(theme)}
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          h6 {
+            ${theme.font.h6.css(theme)}
+          }
+        `}
+      />
       <ScreenClassProvider>{children}</ScreenClassProvider>
     </Fragment>
   )
 }
+
 export const GlobalProvider: React.FC<NeedsChildrenProps & ThemeProps> = ({
   children,
   theme,
