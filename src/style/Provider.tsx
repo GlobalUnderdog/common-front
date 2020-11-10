ed068d8b46d95aecc2a954a2f906055c2a11dcd8
Lukas Danin <lukas.danin@gmail.com>
2020-11-10 16:22:32 -0300

    Removes FontTheme, uses normalize for h1h6
    Exports Google GA

--- src/style/Provider.tsx
+++ src/style/Provider.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import React, { Fragment } from 'react'
 import { ScreenClassProvider, setConfiguration } from 'react-grid-system'
-import { css, Global, jsx, ThemeProvider, useTheme } from './emotion'
+import { Global, jsx, ThemeProvider, useTheme } from './emotion'
 import { ThemeProps } from './theme'
 import { NeedsChildrenProps } from './util'
 
@@ -11,61 +11,6 @@ const Children: React.FC = ({ children }) => {
   return (
     <Fragment>
       <Global styles={theme.global.css(theme)} />
-      <Global styles={theme.font.text.css(theme)} />
-      <Global
-        styles={css`
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            ${theme.font.h1h6.css(theme)}
-          }
-        `}
-      />
-      <Global
-        styles={css`
-          h1 {
-            ${theme.font.h1.css(theme)}
-          }
-        `}
-      />
-      <Global
-        styles={css`
-          h2 {
-            ${theme.font.h2.css(theme)}
-          }
-        `}
-      />
-      <Global
-        styles={css`
-          h3 {
-            ${theme.font.h3.css(theme)}
-          }
-        `}
-      />
-      <Global
-        styles={css`
-          h4 {
-            ${theme.font.h4.css(theme)}
-          }
-        `}
-      />
-      <Global
-        styles={css`
-          h5 {
-            ${theme.font.h5.css(theme)}
-          }
-        `}
-      />
-      <Global
-        styles={css`
-          h6 {
-            ${theme.font.h6.css(theme)}
-          }
-        `}
-      />
       <ScreenClassProvider>{children}</ScreenClassProvider>
     </Fragment>
   )
