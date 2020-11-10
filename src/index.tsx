05e626eab549ece4ed2dd47fe5404c3550dcbcf5
Lukas Danin <lukas.danin@gmail.com>
2020-11-10 15:27:22 -0300

    Uses typed Inspectlet

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
 export * from './components'
 export * from './style'
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const inspectlet = require('./inspectlet.js') as (
-  id: string | number
-) => void
+export * from './inspectlet'
