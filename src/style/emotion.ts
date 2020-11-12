9a67b458d9c1c61069f64481eb9c1e180839758a
Lukas Danin <lukas.danin@gmail.com>
2020-11-12 16:21:28 -0300

    Exports untyped styled

--- src/style/emotion.ts
+++ src/style/emotion.ts
@@ -4,6 +4,7 @@ import { Theme } from './theme'
 
 export { css, Global, jsx, keyframes } from '@emotion/core'
 export { SerializedStyles } from '@emotion/css'
+export { default as untypedStyled } from '@emotion/styled'
 export { ThemeProvider } from 'emotion-theming'
 export const styled = _styled as CreateStyled<Theme>
 export const useTheme = (): Theme => _useTheme<Theme>()
